433133365bcd770ec069fc6db4772042e0888849
it20166038@my.sliit.lk <it20166038@my.sliit.lk>
2021-09-26 20:02:55 +0530

    my git changes

--- app/src/main/java/com/coffee_secrets/CreateActivity.java
+++ app/src/main/java/com/coffee_secrets/CreateActivity.java
@@ -10,12 +10,7 @@ import android.widget.Button;
 public class CreateActivity extends AppCompatActivity {
 
     private Button mPay;
-
-
-
-
-
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create);
